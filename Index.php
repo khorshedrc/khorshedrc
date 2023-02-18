eb9e2548750643dc1ea862f35039fa0112cb7eb5
khorshedrc <khorshedrc@gmail.com>
2023-02-18 16:59:47 +0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My script!<br>"; 
+
+$txt = "PHP";
+echo "I love $txt!<br>";
+
+$x = 5 /* + 15 */ + 5;
+echo $x;
+echo "<br>";
+$txt="World";
+$x=5;
+$y=12;
+echo $x+$y;
+echo "<br>";
+echo $txt;
+
+?>
+
+</body>
+</html>
